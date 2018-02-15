2dbe6f2e53ab92a1e10c98d27af01d9d23b5ab7d
Azur_Azabache <tharuka_20@hotmail.com>
2018-02-15 00:36:30 -0500

    adding modal

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 $(document).ready(()=>{
 
 //variable contador
-let num = 1
+let num = 1;
 //Añadiendo cada imagen
 while( num <= 88 ) {
-let img = `<img class="avatar" src="https://starwars-visualguide.com/assets/img/characters/${num}.jpg">`;
+let img = `<img class="avatar" data-toggle="modal" data-target="#exampleModalCenter" src="https://starwars-visualguide.com/assets/img/characters/${num}.jpg">`;
 $('#container').append(img);
   num++;
 }
+
+
 
 });
